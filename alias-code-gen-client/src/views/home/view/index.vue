13f96b950a4c2dccf751492bde5cf08390c185ce
Zen Gershon <aiilary@163.com>
2024-12-28 22:46:45 +0800

    feat: optimize

--- alias-code-gen-client/src/views/home/view/index.vue
+++ alias-code-gen-client/src/views/home/view/index.vue
@@ -10,34 +10,34 @@
             <a-step v-for="item in steps" :key="item.title" :title="item.title"/>
           </a-steps>
           <div class="steps-content">
-              <code-form v-show="this.current === 0" :cfg="projects" :formRules="projects.rules" ref="projectsRef"/>
+              <code-form v-show="this.current === 0" :cfg="projectCfg" :formRules="projectCfg.rules" ref="projectRef"/>
               <code-table ref="tablesRef" v-show="this.current === 1"/>
               <a-tabs v-show="this.current === 2">
                 <a-tab-pane key="1" tab="数据表">
-                  <code-form :cfg="tableNames" ref="tableNamesRef"/>
+                  <code-form :cfg="tableNameCfg" ref="tableNameRef"/>
                 </a-tab-pane>
                 <a-tab-pane key="2" tab="包名">
-                  <code-form :cfg="packages" ref="packagesRef"/>
+                  <code-form :cfg="packageCfg" ref="packageRef"/>
                 </a-tab-pane>
                 <a-tab-pane key="3" tab="文件名">
-                  <code-form :cfg="fileNames" ref="fileNamesRef"/>
+                  <code-form :cfg="fileNameCfg" ref="fileNameRef"/>
                 </a-tab-pane>
                 <a-tab-pane key="4" tab="父类">
-                  <code-form :cfg="superClasses" ref="superClassesRef"/>
+                  <code-form :cfg="superClassCfg" ref="superClassRef"/>
                 </a-tab-pane>
               </a-tabs>
               <a-collapse v-model="activeKey" v-show="this.current === 3">
                 <a-collapse-panel key="1" header="全局策略" :showArrow="false" disabled>
-                  <code-form :cfg="globalStrategy" ref="globalStrategyRef"/>
+                  <code-form :cfg="globalStrategyCfg" ref="globalStrategyRef"/>
                 </a-collapse-panel>
                 <a-collapse-panel key="2" header="Entity策略" :showArrow="false" disabled>
-                  <code-form :cfg="entityStrategy" ref="entityStrategyRef"/>
+                  <code-form :cfg="entityStrategyCfg" ref="entityStrategyRef"/>
                 </a-collapse-panel>
                 <a-collapse-panel key="3" header="Controller策略" :showArrow="false" disabled>
-                  <code-form :cfg="ctrlStrategy" ref="ctrlStrategyRef"/>
+                  <code-form :cfg="ctrlStrategyCfg" ref="ctrlStrategyRef"/>
                 </a-collapse-panel>
                 <a-collapse-panel key="4" header="Mapper策略" :showArrow="false" disabled>
-                  <code-form :cfg="mapperStrategy" ref="mapperStrategyRef"/>
+                  <code-form :cfg="mapperStrategyCfg" ref="mapperStrategyRef"/>
                 </a-collapse-panel>
               </a-collapse>
               <div v-show="this.current === 4">
@@ -49,7 +49,7 @@
             <a-button v-if="current < steps.length - 1" type="primary" @click="next">
               下一步
             </a-button>
-            <a-button v-if="current == steps.length - 1" type="danger" @click="generate()">
+            <a-button v-if="current === steps.length - 1" type="danger" @click="generate()">
               生成代码
             </a-button>
             <a-button v-if="current > 0" style="margin-left: 8px" @click="prev">
@@ -108,45 +108,45 @@ export default {
       steps: [{title: "项目配置"}, {title: "选择数据表"}, {title: "个性化配置"}, {title: "策略配置"}, {title: "生成代码"}],
       activeKey: ["1", "2", "3", "4"],
       initList: {},
-      projects: {
+      projectCfg: {
         layout: formItemLayout,
         itemList: [],
         rules: projectRules,
         span: 24,
       },
-      tableNames: {
+      tableNameCfg: {
         layout: formItemLayout,
         itemList: [],
         span: 24,
       },
-      packages: {
+      packageCfg: {
         layout: formItemLayout,
         itemList: [],
         span: 24,
       },
-      fileNames: {
+      fileNameCfg: {
         layout: formItemLayout,
         itemList: [],
         span: 24,
       },
-      superClasses: {
+      superClassCfg: {
         layout: formItemLayout,
         itemList: [],
         span: 24,
       },
-      globalStrategy: {
+      globalStrategyCfg: {
         layout: switchLayout,
         itemList: [],
       },
-      entityStrategy: {
+      entityStrategyCfg: {
         layout: switchLayout,
         itemList: [],
       },
-      ctrlStrategy: {
+      ctrlStrategyCfg: {
         layout: switchLayout,
         itemList: [],
       },
-      mapperStrategy: {
+      mapperStrategyCfg: {
         layout: switchLayout,
         itemList: [],
       },
@@ -158,91 +158,91 @@ export default {
   },
   methods: {
     async init() {
-      this.projects.itemList = [
-        { type: "input", label: "项目路径", prop: "projectLocation", help: "生成代码的项目路径，配置到项目根目录即可", },
-        { type: "input", label: "开发者", prop: "developer", help: "开发人员名称", },
-        { type: "input", label: "包名", prop: "packageName",  help: "业务模块包路径，例如：com.yizlan.**.**.base", },
-        { type: "select", label: "数据库类型", prop: "dbType", },
-        { type: "input", label: "地址", prop: "host", help: "数据库地址格式：ip:port", },
-        { type: "input", label: "schema", prop: "schema", help: "数据库schema, 部分数据库适用", },
-        { type: "input", label: "数据库", prop: "dbName", help: "数据库的名称", },
-        { type: "input", label: "用户名", prop: "userName", help: "数据库的用户名", },
-        { type: "input", label: "密码", prop: "password", help: "数据库的密码", },
+      this.projectCfg.itemList = [
+        { type: "input", label: "项目路径", prop: "projectLocation", help: "生成代码的项目路径，配置到项目根目录即可" },
+        { type: "input", label: "开发者", prop: "developer", help: "开发人员名称" },
+        { type: "input", label: "包名", prop: "packageName",  help: "业务模块包路径，例如：com.yizlan.**.**.base" },
+        { type: "select", label: "数据库类型", prop: "dbType" },
+        { type: "input", label: "地址", prop: "host", help: "数据库地址格式：ip:port" },
+        { type: "input", label: "schema", prop: "schema", help: "数据库schema, 部分数据库适用" },
+        { type: "input", label: "数据库", prop: "dbName", help: "数据库的名称" },
+        { type: "input", label: "用户名", prop: "userName", help: "数据库的用户名" },
+        { type: "input", label: "密码", prop: "password", help: "数据库的密码" },
         { type: "select", label: "Xml", prop: "xmlTargetDir", help: "生成XML的目录",
-        },
+        }
       ]
 
-      this.tableNames.itemList = [
-        { type: "input", label: "数据表前缀", prop: "tablePrefix", help: "忽略的表前缀，生成后的实体类名会忽略前缀", },
-        { type: "input", label: "数据表后缀", prop: "tableSuffix", help: "忽略的表后缀，生成后的实体类名会忽略后缀", },
-        { type: "input", label: "表字段前缀", prop: "tableFieldPrefix", help: "忽略的表字段前缀，生成后的实体类字段名会忽略前缀", },
-        { type: "input", label: "表字段后缀", prop: "tableFieldSuffix", help: "忽略的表字段后缀，生成后的实体类字段名会忽略后缀", },
+      this.tableNameCfg.itemList = [
+        { type: "input", label: "数据表前缀", prop: "tablePrefix", help: "忽略的表前缀，生成后的实体类名会忽略前缀" },
+        { type: "input", label: "数据表后缀", prop: "tableSuffix", help: "忽略的表后缀，生成后的实体类名会忽略后缀" },
+        { type: "input", label: "表字段前缀", prop: "tableFieldPrefix", help: "忽略的表字段前缀，生成后的实体类字段名会忽略前缀" },
+        { type: "input", label: "表字段后缀", prop: "tableFieldSuffix", help: "忽略的表字段后缀，生成后的实体类字段名会忽略后缀" }
       ]
 
-      this.packages.itemList = [
+      this.packageCfg.itemList = [
         { type: "input", label: "实体类", prop: "packageEntity", help: "默认: entity" },
         { type: "input", label: "Controller", prop: "packageCtrl", help: "默认: controller" },
         { type: "input", label: "Service", prop: "packageService", help: "默认: service" },
         { type: "input", label: "ServiceImpl", prop: "packageServiceImpl", help: "默认: service.impl" },
         { type: "input", label: "Mapper ", prop: "packageMapper", help: "默认: mapper" },
-        { type: "input", label: "Xml", prop: "packageMapperXml", help: "默认: mapper.xml" },
+        { type: "input", label: "Xml", prop: "packageMapperXml", help: "默认: mapper.xml" }
       ]
 
-      this.fileNames.itemList = [
+      this.fileNameCfg.itemList = [
         { type: "input", label: "实体类", prop: "fileNameEntity", help: "默认: %s" },
         { type: "input", label: "Controller", prop: "fileNameCtrl", help: "默认: %sController" },
         { type: "input", label: "Service", prop: "fileNameService", help: "默认: I%sService" },
         { type: "input", label: "ServiceImpl", prop: "fileNameServiceImpl", help: "默认: %sServiceImpl" },
         { type: "input", label: "Mapper", prop: "fileNameMapper", help: "默认: %sMapper" },
-        { type: "input", label: "Xml", prop: "fileNameMapperXml", help: "默认: %sMapper" },
+        { type: "input", label: "Xml", prop: "fileNameMapperXml", help: "默认: %sMapper" }
       ]
 
-      this.superClasses.itemList = [
+      this.superClassCfg.itemList = [
         { type: "input", label: "实体类", prop: "superClassEntity", help: "example: com.*...*.BaseEntity" },
         { type: "input", label: "Controller", prop: "superClassCtrl", help: "example: com.*...*.BaseController" },
         { type: "input", label: "Service", prop: "superClassService", help: "example: com.*...*.BaseService" },
         { type: "input", label: "ServiceImpl", prop: "superClassServiceImpl", help: "example: com.*...*.BaseServiceImpl" },
-        { type: "input", label: "Mapper", prop: "superClassMapper", help: "example: com.*...*.BaseMapper" },
+        { type: "input", label: "Mapper", prop: "superClassMapper", help: "example: com.*...*.BaseMapper" }
       ];
 
-      this.globalStrategy.itemList = [
+      this.globalStrategyCfg.itemList = [
         { type: "switch", label: "Kotlin项目", prop: "kotlinProject" },
-        { type: "switch", label: "Swagger文档", prop: "useSwagger" },
+        { type: "switch", label: "Swagger文档", prop: "useSwagger" }
       ]
 
-      this.entityStrategy.itemList = [
+      this.entityStrategyCfg.itemList = [
         { type: "switch", label: "表注解", prop: "useTableAnnotation" },
-        { type: "switch", label: "字段注解", prop: "useTableFieldAnnotation", },
+        { type: "switch", label: "字段注解", prop: "useTableFieldAnnotation" },
         { type: "switch", label: "禁用序列化", prop: "disableSerialization" },
-        { type: "switch", label: "lombok注解", prop: "useLombok" },
+        { type: "switch", label: "lombok注解", prop: "useLombok" }
       ]
 
-      this.ctrlStrategy.itemList = [
+      this.ctrlStrategyCfg.itemList = [
         { type: "switch", label: "URL使用驼峰转连字符", prop: "useHyphen" },
-        { type: "switch", label: "Rest风格", prop: "useRestStyle" },
+        { type: "switch", label: "Rest风格", prop: "useRestStyle" }
       ]
 
-      this.mapperStrategy.itemList = [
-        { type: "switch", label: "BaseResultMap", prop: "generateBaseResultMap", },
-        { type: "switch", label: "BaseColumnList", prop: "generateBaseColumnList", },
-        { type: "switch", label: "Mapper注解", prop: "useMapperAnnotation", },
+      this.mapperStrategyCfg.itemList = [
+        { type: "switch", label: "BaseResultMap", prop: "generateBaseResultMap" },
+        { type: "switch", label: "BaseColumnList", prop: "generateBaseColumnList" },
+        { type: "switch", label: "Mapper注解", prop: "useMapperAnnotation" }
       ]
 
       const data = await api.initialize()
       if (data === false) return
-      this.$refs.projectsRef.setOptions('dbType', formatListToOptions(data.dbTypes, 'itemText', 'itemValue'))
-      this.$refs.projectsRef.setOptions('xmlTargetDir', formatListToOptions(data.xmlDirs, 'itemText', 'itemValue'))
+      this.$refs.projectRef.setOptions('dbType', formatListToOptions(data.dbTypes, 'itemText', 'itemValue'))
+      this.$refs.projectRef.setOptions('xmlTargetDir', formatListToOptions(data.xmlDirs, 'itemText', 'itemValue'))
     },
     async next() {
       if (this.current === 0) {
-        const valid = await this.$refs.projectsRef.validateForm();
+        const valid = await this.$refs.projectRef.validateForm();
         if (!valid) {
           return
         }
       }
       this.current++
       if (this.current === 1) {
-        const param = this.$refs.projectsRef.getFormModel();
+        const param = this.$refs.projectRef.getFormModel();
         const tables = await api.searchTables(param)
         this.$refs.tablesRef.setDataSource(tables);
         Object.assign(this.formModel, param);
@@ -254,24 +254,24 @@ export default {
         Object.assign(this.formModel, tables)
       }
       if (this.current === 3) {
-        const tableNames = this.$refs.tableNamesRef.getFormModel();
+        const tableNames = this.$refs.tableNameRef.getFormModel();
         Object.assign(this.formModel, tableNames)
 
-        const packagesRef = this.$refs.packagesRef
-        if (packagesRef) {
-          const packages = packagesRef.getFormModel();
+        const packageRef = this.$refs.packageRef
+        if (packageRef) {
+          const packages = packageRef.getFormModel();
           Object.assign(this.formModel, packages)
         }
 
-        const fileNamesRef = this.$refs.fileNamesRef
-        if (fileNamesRef) {
-          const fileNames = fileNamesRef.getFormModel();
+        const fileNameRef = this.$refs.fileNameRef
+        if (fileNameRef) {
+          const fileNames = fileNameRef.getFormModel();
           Object.assign(this.formModel, fileNames)
         }
 
-        const superClassesRef = this.$refs.superClassesRef
-        if (superClassesRef) {
-          const superClasses = superClassesRef.getFormModel();
+        const superClassRef = this.$refs.superClassRef
+        if (superClassRef) {
+          const superClasses = superClassRef.getFormModel();
           Object.assign(this.formModel, superClasses)
         }
       }
